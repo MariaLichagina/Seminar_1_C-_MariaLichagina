001d18d3195b1450ba3e5bb1bcf496447a2ef782
Maria L <maria_lich@mail.ru>
2023-05-24 23:27:28 +0300

    tasks done

---
+++ Task 4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.WriteLine("Введите  число:");
+int num = int.Parse(Console.ReadLine());
+
+for (int count = 2; count <= num; count += 2)
+    Console.Write($"{count} ");
+
+
